a948e994623788473c72fa161640d374b83fc43f
Colton Somes <coltonsomes@Coltons-MacBook-Air.local>
2017-07-18 10:57:22 -0700

    message

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,7 +17,7 @@ import FilterModal from './FilterModal.js';
 export default class HomeScreen extends React.Component {
 
   static navigationOptions = {
-    title: 'Homie',
+    title: 'Title',
     header: null
   };
 
